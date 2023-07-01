9a75ceb1b14bbcb005788af36aa6d54ee7e2f83d
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 15:17:20 -0500

    age ture false using boolean

---
+++ 1.Intro to Python/boolean.py
@@ -0,0 +1,9 @@
+age = 20
+is_over_age = age >= 18 # true
+is_under_age = age < 18 # False not using less then or equal to
+is_twenty = age == 20 # single equal sign used for assignment two equal signs used for comparission
+
+my_number = 5 
+user_number = int(input("Enter the value :"))
+matches = my_number == user_number
+print(f"You got it right {matches}.")
