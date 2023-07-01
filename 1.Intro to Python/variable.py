3aaee515c5961be721e38a18ec48cf10ecaedbbd
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 13:44:26 -0500

    variables numbers

---
+++ 1.Intro to Python/variable.py
@@ -0,0 +1,10 @@
+age =20
+print(age)
+
+age_vaeriable = 30  #snake_case variable declaration
+print(age)
+
+PI = 3.14159   # never going to change in future, writing in capital letters
+RAIANS_TO_DEGREES = 180/PI  
+
+print(PI)
