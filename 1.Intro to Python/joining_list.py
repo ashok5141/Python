56c02efe50d83bb58d0c7fa8d95264940addb5fc
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 18:17:57 -0500

    some notes--joining list

---
+++ 1.Intro to Python/joining_list.py
@@ -0,0 +1,3 @@
+friends = ["Ashok","Bob","Anne"]
+comma_separated = ", ".join(friends)
+print(f"My friends are : {comma_separated}.")
--- 1.Intro to Python/lengthANDsum.py
+++ 1.Intro to Python/lengthANDsum.py
@@ -1,6 +1,6 @@
 grades = [80,75,90,100] #list
-#grades = (80,75,90,100) # tuple
-#grades = {80,75,90,100} #set
+#grades = (80,75,90,100) # tuple BAD choise cant add middle of program
+#grades = {80,75,90,100} #set WOREST set one of them removed
 total = sum(grades)
 length = len(grades)
 average = total / length
