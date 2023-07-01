4d4253d74baa84fe6c64e89fc8d30506d4f5b3b6
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 13:48:12 -0500

    variable and numbers

---
+++ 1.Intro to Python/numbers.py
@@ -0,0 +1,15 @@
+age = 20 # number
+PI = 3.14159 # float
+
+maths_operations = 1 + 3 * 4 / 2 - 2
+print("maths_operations : ",maths_operations)
+
+interger_division = 8 // 3 # it will provide interger value instead of float if you use //
+print("interger_division : ",interger_division)
+
+remainder = 15 // 2
+print("remainder 1 : ",remainder)
+
+x = 37
+remainder = x % 2 # reuse variable 
+print("remainder 2 : ", remainder)
