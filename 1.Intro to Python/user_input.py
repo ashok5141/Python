9bfb8e32db82efb7c1579082bae260e44d18c9e5
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 15:20:17 -0500

    user input added converted to int

--- 1.Intro to Python/user_input.py
+++ 1.Intro to Python/user_input.py
@@ -4,8 +4,8 @@ print(f"Hello {your_name}. My name is {my_name}")
 
 age = input("Enter your age : ")
 print(f"You have lived for {age * 12} months") #it printing string age 12 times
-age_num = int(age)
+age_num = int(age) # the age_num useless because using only below state, use below example, directly convering interger
 print(f"You have lived for {age_num * 12} months") # here we are converting the age variable into interger
 
-age = int(input("Enter your age :"))
-print(f"You have lived for {age * 12} months") #converting the user input into interger
+age = int(input("Enter your age again :")) # python will execute inside bracket run first, after pressing enter it will conver into interger
+print(f"You have lived for  {age * 12} months") #converting the user input into interger
