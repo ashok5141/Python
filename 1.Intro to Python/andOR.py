f81244c7f63fedce93bb565f4ce15a6d4f04443b
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 15:41:21 -0500

    user not providing input, print default value .

--- 1.Intro to Python/andOR.py
+++ 1.Intro to Python/andOR.py
@@ -7,7 +7,7 @@ age = 0
 user_age = age or default_age
 print(user_age)
 
-default_greeting = "There !"
+default_greeting = "There !"   # printing default name, if user is not provided actual name
 name = input("Enter your name: (optional) ")
 user_name = name or default_greeting
 print(f"Hello, {user_name}!.")
