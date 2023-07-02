095b356a82e213ed4e3d216d2de3b61ac737e07a
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-01 20:13:48 -0500

    Destructuring of syntax in for loop

---
+++ 2.python_Fundamentals/destructuring_syntax.py
@@ -0,0 +1,12 @@
+currencies = 0.8, 1.2
+usd, eur = currencies
+
+friends = [("Ashok",27),("Bob",17),("Anne",7),("Kumar",0)]
+for name, age in friends:
+    print(f"{name} is {age} years old.")
+
+/*
+
+
+*/
+
