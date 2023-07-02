148966e100058544233e5d91d69e7b803c11b01f
ASHOK REDDY <ashokreddymedikonda@gmail.com>
2023-07-02 15:22:24 -0500

    declaring a fucntion

---
+++ 2.python_Fundamentals/funtion.py
@@ -0,0 +1,6 @@
+def greet():
+    name = input("Enter your name : ")
+    print(f"Hello, {name}")
+greet() # call the function after declaration
+
+#print(name) the object NAME declared inside the function but it's not accessiable outside 
